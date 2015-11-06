891060747145ab384a3c71981c67480d1535ca6a
Boaz Sobrado <sobradob@gmail.com>
2015-11-05 21:38:50 -0800

    added function code

---
+++ GoogleMapsAPIDir.R
@@ -0,0 +1,33 @@
+#distance calucations
+#### This script uses RCurl and RJSONIO to download data from Google's API
+
+library(RCurl)
+library(RJSONIO)
+
+#define api key as: api<-"YOURAPI"
+#the function assumes address input in following dataframe:
+#lat1,lng1,lat2,lng2
+
+DistDur <- function(address,time = as.integer(Sys.time()),mode, return.call = "json", sensor = "false") {
+  address1<-paste(address[,1],address[,2], sep=" ")
+  address2<-paste(address[,3],address[,4], sep=" ")
+  root <- "https://maps.googleapis.com/maps/api/directions/"
+  u <- paste(root, return.call, "?origin=", address1, "&destination=",address2,"&departure_time=",time,"&mode=",mode,"&key=",api,sep = "")
+  for (a in 1:length(u)){
+    url<-URLencode(u[a])
+    doc <- getURL(url)
+    x <- fromJSON(doc,simplify = FALSE)
+    dist<-0
+    dura<-0
+    for (i in 1:length(x$routes[[1]]$legs[[1]]$steps))
+    {
+      dist<-x$routes[[1]]$legs[[1]]$steps[[i]]$distance$value+dist
+      dura<-x$routes[[1]]$legs[[1]]$steps[[i]]$duration$value+dura
+    }
+    address$dura[a]<-dura
+    address$dist[a]<-dist
+  }
+  return(address)
+}
+
+
